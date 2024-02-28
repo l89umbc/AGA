18408440a21a755f2366cb1974db61169f276a5d
Landon <l89@umbc.edu>
2024-02-28 15:40:05 -0500

    added navigation to exam scan

--- app/src/main/java/com/example/anonymousgradingapplication/AddExamActivity.java
+++ app/src/main/java/com/example/anonymousgradingapplication/AddExamActivity.java
@@ -2,13 +2,26 @@ package com.example.anonymousgradingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddExamActivity extends AppCompatActivity {
 
+    private Button addExamButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exam);
+
+        addExamButton = (Button) findViewById(R.id.buttonAddExam);
+        addExamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AddExamActivity.this, ExamScanActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/anonymousgradingapplication/SelectedCourseActivity.java
+++ app/src/main/java/com/example/anonymousgradingapplication/SelectedCourseActivity.java
@@ -2,7 +2,9 @@ package com.example.anonymousgradingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SelectedCourseActivity extends AppCompatActivity {
@@ -10,11 +12,25 @@ public class SelectedCourseActivity extends AppCompatActivity {
     private Button classInfoButton;
     private Button examsButton;
     private Button saveButton;
+    private Button uploadButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_course);
+
+        classInfoButton = (Button) findViewById(R.id.buttonClassInfo);
+        examsButton = (Button) findViewById(R.id.buttonExams);
+        saveButton = (Button) findViewById(R.id.buttonCourseSave);
+        uploadButton = (Button) findViewById(R.id.buttonUploadRoster);
+
+        examsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SelectedCourseActivity.this, AddExamActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
