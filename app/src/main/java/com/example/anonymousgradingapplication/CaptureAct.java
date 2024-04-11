5c99961a78d849218496ed69468c7aaffdf78328
RileySheehy <rileysheehy@yahoo.com>
2024-04-11 17:09:35 -0400

    Barcode Scanner

---
+++ app/src/main/java/com/example/anonymousgradingapplication/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.anonymousgradingapplication;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+public class CaptureAct extends ExamScanActivity{
+
+}
--- app/src/main/java/com/example/anonymousgradingapplication/ExamScanActivity.java
+++ app/src/main/java/com/example/anonymousgradingapplication/ExamScanActivity.java
@@ -1,12 +1,13 @@
 package com.example.anonymousgradingapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class ExamScanActivity extends AppCompatActivity {
 
@@ -21,21 +22,33 @@ public class ExamScanActivity extends AppCompatActivity {
         buttonCamera = (Button) findViewById(R.id.buttonCamera);
         backButton = (Button) findViewById(R.id.buttonExamScanBack);
 
-        buttonCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(myIntent);
-            }
-        });
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
+        backButton.setOnClickListener(v -> finish());
+        buttonCamera.setOnClickListener(v->
+        {
+            scanCode();
         });
     }
 
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
 
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->{
+        if (result.getContents() != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(ExamScanActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 }
